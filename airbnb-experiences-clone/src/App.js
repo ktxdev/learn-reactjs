47b823681f24e047a4dad8c7c65f896cb5ecce1d
ktxdev <sean.ktxdev@gmail.com>
2022-04-05 20:55:41 +0200

    Pass values to Card component as props

--- airbnb-experiences-clone/src/App.js
+++ airbnb-experiences-clone/src/App.js
@@ -3,11 +3,27 @@ import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 
 function App() {
+
+  const experience = {
+    img: "https://images.unsplash.com/photo-1531159243698-b48e4c40c228?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+    rating: 5.0,
+    reviewCount: 6,
+    country: "USA",
+    title: "Life lessons with Katie",
+    price: 136
+  }
+
   return (
     <div className="App">
       <Navbar />
       <Hero />
-      <Card />
+      <Card 
+        img={experience.img} 
+        rating={experience.rating} 
+        reviewCount={experience.reviewCount} 
+        country={experience.country} 
+        title={experience.title} 
+        price={experience.price} />
     </div>
   );
 }
--- airbnb-experiences-clone/src/components/Card.jsx
+++ airbnb-experiences-clone/src/components/Card.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
 import starIcon from '../images/star.png'
 
-const Card = () => {
+const Card = ({ img, rating, reviewCount, country, title, price }) => {
   return (
     <div className='card'> 
-        <img className='card--image' src="https://images.unsplash.com/photo-1531159243698-b48e4c40c228?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80" alt="Image" />
+        <img className='card--image' src={img} alt="Image" />
         <div className='card--stats'>
             <img className='card--star' src={starIcon} alt="Star Icon" />
-            <span>5.0</span>
-            <span className='gray'>(6) </span>
-            <span className='gray'>USA</span>
+            <span>{rating}</span>
+            <span className='gray'>({reviewCount}) </span>
+            <span className='gray'>{country}</span>
         </div>
-        <p className='card--text'>Life lessons with Katie</p>
-        <p className='card--text'><span className='bold'>From $145</span> / person</p>
+        <p className='card--text'>{title}</p>
+        <p className='card--text'><span className='bold'>From {`$${price}`}</span> / person</p>
     </div>
   )
 }
