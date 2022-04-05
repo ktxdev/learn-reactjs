dad2fb3de864040639d2ead9d61524ee1ea41b18
ktxdev <sean.ktxdev@gmail.com>
2022-04-05 21:24:02 +0200

    retrieve info from data file

--- airbnb-experiences-clone/src/App.js
+++ airbnb-experiences-clone/src/App.js
@@ -1,29 +1,26 @@
 import Card from "./components/Card";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
+import { data } from "./data.js";
 
 function App() {
-
-  const experience = {
-    img: "https://images.unsplash.com/photo-1531159243698-b48e4c40c228?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
-    rating: 5.0,
-    reviewCount: 6,
-    country: "USA",
-    title: "Life lessons with Katie",
-    price: 136
-  }
 
   return (
     <div className="App">
       <Navbar />
       <Hero />
-      <Card 
-        img={experience.img} 
-        rating={experience.rating} 
-        reviewCount={experience.reviewCount} 
-        country={experience.country} 
-        title={experience.title} 
-        price={experience.price} />
+      <section className="cards-list">
+        {
+          data.map(d => <Card
+            key={d.id}
+            img={d.img}
+            rating={d.rating}
+            reviewCount={d.reviewCount}
+            country={d.country}
+            title={d.title}
+            price={d.price} />)
+        }
+      </section>
     </div>
   );
 }
---
+++ airbnb-experiences-clone/src/data.js
@@ -0,0 +1,38 @@
+export const data = [
+    {
+        id: 1,
+        img: "https://images.unsplash.com/photo-1531159243698-b48e4c40c228?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+        rating: 5.0,
+        reviewCount: 6,
+        country: "USA",
+        title: "Life lessons with Katie",
+        price: 136
+    },
+    {
+        id: 2,
+        img: "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+        rating: 7.0,
+        reviewCount: 10,
+        country: "South Africa",
+        title: "Life lessons with Katie",
+        price: 300
+    },
+    {
+        id: 3,
+        img: "https://images.unsplash.com/photo-1507114845806-0347f6150324?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+        rating: 10.0,
+        reviewCount: 5,
+        country: "Zimbabwe",
+        title: "Life lessons with Katie",
+        price: 456
+    },
+    {
+        id: 4,
+        img: "https://images.unsplash.com/photo-1481824429379-07aa5e5b0739?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=696&q=80",
+        rating: 2.0,
+        reviewCount: 3,
+        country: "Egypt",
+        title: "Life lessons with Katie",
+        price: 124
+    }
+]
