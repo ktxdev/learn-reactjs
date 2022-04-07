895f24a19decfaec23dd204c27308e11cc869cc4
ktxdev <sean.ktxdev@gmail.com>
2022-04-07 20:35:35 +0200

    Created header component

--- contact-book/src/App.jsx
+++ contact-book/src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Header from './components/Header';
 
 const App = () => {
   return (
-    <div>
-      Hello
+    <div className='app'>
+      <Header/>
     </div>
   );
 }
---
+++ contact-book/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <section className='header'>
+        <h2 className='header--title'>Contact Book</h2>
+        <button className='header--btn' >New Contact</button>
+    </section>
+  )
+}
+
+export default Header
